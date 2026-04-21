1e831a9499dc0a798770b0f1475b4d97eb0f71e9
Tytoyos <jonathan.eich07@gmail.com>
2026-04-21 16:00:26 +0200

    1.21.10 version

--- src/main/kotlin/impl/partycommands/ForcefemCommand.kt
+++ src/main/kotlin/impl/partycommands/ForcefemCommand.kt
@@ -9,7 +9,7 @@ class ForcefemCommand : PartyCommand {
         val target = args[0]
         val femroll = (1..10).random()
         val amount = listOf("100mg", "1mg", "50mg", "1000mg", "500mg").random()
-        val responses = listOf("Laced $target's drink with $amount of estrogen!", "No","$target is already beyond that stage.",
+        val responses = listOf("Laced $target's drink with $amount of estrogen!", "No.","$target is already beyond that stage.",
                                 "Secretly putting a syringe with $amount of estrogen in $target's ass!!")
 
         val responseskaiine = listOf("ESTROGEN OVERLOAD!!!!", "UH OH, I ACCIDENTALLY STABBED HIS HEART. HE DED")
@@ -21,7 +21,7 @@ class ForcefemCommand : PartyCommand {
         // add easter egg for moocwazy
 
         if ((1..20).random() == 5) {
-            return "DIVINE INTERVENTION!!! $target HAS BEEN TURNED INTO THE TRUE EMBODIMENT OF ESTROGEN!!!!!"
+            return "DIVINE INTERVENTION!!! ${target.uppercase()} HAS BEEN TURNED INTO THE TRUE EMBODIMENT OF ESTROGEN!!!!!"
         }
 
         return responses.random()
